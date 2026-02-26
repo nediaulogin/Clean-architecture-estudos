2fb309411d274121a8d606c2b673927d7887328d
Elvis Sousa <nediaulog@gmail.com>
2026-02-26 14:17:30 -0300

    add stock property to Product entity with methods to increase and decrease stock

--- src/domain/Product/entity/product.ts
+++ src/domain/Product/entity/product.ts
@@ -2,7 +2,7 @@ export type ProductProps = {
   id: string;
   name: string;
   price: number;
-
+  stock: number; // ← novo campo persistido
 };
 
 export class Product {
@@ -10,14 +10,35 @@ export class Product {
 
   public static create(name: string, price: number): Product {
     return new Product({
-      id: crypto.randomUUID().toString(),
+      id: crypto.randomUUID(),
       name,
       price,
-    })
-  };
+      stock: 0, // inicia zerado
+    });
+  }
 
   public static with(props: ProductProps) {
     return new Product(props);
+  }
+
+  public increaseStock(quantity: number) {
+    if (quantity <= 0) {
+      throw new Error("Quantity must be greater than zero");
+    }
+
+    this.props.stock += quantity;
+  }
+
+  public decreaseStock(quantity: number) {
+    if (quantity <= 0) {
+      throw new Error("Quantity must be greater than zero");
+    }
+
+    if (this.props.stock < quantity) {
+      throw new Error("Insufficient stock");
+    }
+
+    this.props.stock -= quantity;
   }
 
   public get id() {
@@ -32,7 +53,7 @@ export class Product {
     return this.props.price;
   }
 
-
-
-
+  public get stock() {
+    return this.props.stock;
+  }
 }
